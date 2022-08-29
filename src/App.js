7c18360ffed9301f149e980d4b7162155f33152b
Alexis Santa Ana <ibanez2112@hotmail.com>
2022-08-29 11:08:10 -0300

    quilombo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import 'mdb-ui-kit/js/mdb.min.js';
 import 'mdb-ui-kit/css/mdb.min.css';
+import 'rodal/lib/rodal.css'
 import Footer from "./components/Footer";
 import Main from "./components/Main";
 import Navbar from "./components/Navbar";
