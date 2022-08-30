edbab79666d44097ed317a42a286c92db3758a41
Alexis Santa Ana <ibanez2112@hotmail.com>
2022-08-30 16:23:51 -0300

    fix??

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react'
-import './styles.css'
-import Swal from 'sweetalert2'
-import 'animate.css';
-import AOS from 'aos';
+import React, { useEffect } from "react"
+import "./styles.css"
+import Swal from "sweetalert2"
+import "animate.css";
+import AOS from "aos";
 import "aos/dist/aos.css";
 
 const Contact = () => {
@@ -24,10 +24,10 @@ const Contact = () => {
       showConfirmButton: false,
       timer: 4000,
       showClass: {
-        popup: 'animate__animated animate__fadeInUp'
+        popup: "animate__animated animate__fadeInUp"
       },
       hideClass: {
-        popup: 'animate__animated animate__fadeOutDown'
+        popup: "animate__animated animate__fadeOutDown"
       }
     });
   }
@@ -42,32 +42,32 @@ const Contact = () => {
 
   return (
     <>
-      <h2 className='pt-5' data-aos="fade-right" data-aos-duration="800">CONTACTO</h2>
-      <div className='bar' data-aos="fade-left" data-aos-duration="800"></div>
-      <h5 className='py-1 preguntas text-center' data-aos="fade-up" data-aos-duration="800"><i>¿Alguna pregunta o querés que <span>trabajemos juntos?</span></i></h5>
-      <form name='contact v1' method='post' netlify className='form' onSubmit={e => handleSubmit(e)} data-aos="fade-up" data-aos-duration="800">
-        <div className='row'>
+      <h2 className="pt-5" data-aos="fade-right" data-aos-duration="800">CONTACTO</h2>
+      <div className="bar" data-aos="fade-left" data-aos-duration="800"></div>
+      <h5 className="py-1 preguntas text-center" data-aos="fade-up" data-aos-duration="800"><i>¿Alguna pregunta o querés que <span>trabajemos juntos?</span></i></h5>
+      <form name="contact" method="POST" data-netlify="true" className="form" onSubmit={e => handleSubmit(e)} data-aos="fade-up" data-aos-duration="800">
+        <div className="row">
           {/* hidden input */}
           <input type="hidden" name="form-name" value="contact v1"></input>
           {/* name */}
-          <div className='col-12 col-sm-6 d-flex flex-column'>
-            <label htmlFor='name'><b>Nombre completo</b></label>
-            <input name='name' type="name" placeholder="Ingrese su nombre" id='name' required></input>
+          <div className="col-12 col-sm-6 d-flex flex-column">
+            <label htmlFor="name"><b>Nombre completo</b></label>
+            <input name="name" type="name" placeholder="Ingrese su nombre" id="name" required></input>
           </div>
           {/* email */}
-          <div className='col-12 col-sm-6 d-flex flex-column'>
-            <label htmlFor='email'><b>Email</b></label>
-            <input name="email" type="email" placeholder="Ingrese su email" id='email' required></input>
+          <div className="col-12 col-sm-6 d-flex flex-column">
+            <label htmlFor="email"><b>Email</b></label>
+            <input name="email" type="email" placeholder="Ingrese su email" id="email" required></input>
           </div>
         </div>
-        <div className='row'>
+        <div className="row">
           {/* message */}
-          <div className='col-12 d-flex flex-column'>
+          <div className="col-12 d-flex flex-column">
             <label htmlFor="msg"><b>Mensaje</b></label>
-            <textarea name='msg' id='msg' placeholder='Escriba su mensaje' style={{ resize: "none" }} rows="8" cols="20" required />
+            <textarea name="msg" id="msg" placeholder="Escriba su mensaje" style={{ resize: "none" }} rows="8" cols="20" required />
           </div>
         </div>
-        <button type='submit' className='btn btn-dark my-2 w-100'>Enviar!</button>
+        <button type="submit" className="btn btn-dark my-2 w-100">Enviar!</button>
       </form>
     </>
   )
