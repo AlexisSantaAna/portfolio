65210c418e4a2fa894f7f73249f7cf141cacfa99
Alexis Santa Ana <ibanez2112@hotmail.com>
2022-08-30 20:45:19 -0300

    final?

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import "./styles.css"
 import Swal from "sweetalert2"
 import "animate.css";
@@ -6,35 +6,38 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 const Contact = () => {
+  const [hasSubmited, setHasSubmited] = useState(false)
 
   //AOS library
   useEffect(() => {
     AOS.init({ once: true });
     AOS.refresh();
-  }, []);
+  }, []); 
 
-  const notification = () => {
-    //Sweet Alert 2 library
-    Swal.fire({
-      toast: true,
-      position: "bottom-end",
-      icon: "success",
-      iconColor: "#4da4ea",
-      title: `Mensaje enviado con éxito!`,
-      showConfirmButton: false,
-      timer: 4000,
-      showClass: {
-        popup: "animate__animated animate__fadeInUp"
-      },
-      hideClass: {
-        popup: "animate__animated animate__fadeOutDown"
-      }
-    });
-  }
+  useEffect(() => {
+    if (hasSubmited) {
+      //Sweet Alert 2 library
+      Swal.fire({
+        toast: true,
+        position: "bottom-end",
+        icon: "success",
+        iconColor: "#4da4ea",
+        title: `Mensaje enviado con éxito!`,
+        showConfirmButton: false,
+        timer: 4000,
+        showClass: {
+          popup: "animate__animated animate__fadeInUp"
+        },
+        hideClass: {
+          popup: "animate__animated animate__fadeOutDown"
+        }
+      });
+      setHasSubmited(false)
+    }
+  }, [])
 
-  const handleSubmit = (e) => {
-    notification()
-    // e.target.reset()
+  const handleSubmit = () => {
+    setHasSubmited(true)
   }
 
   return (
@@ -42,7 +45,7 @@ const Contact = () => {
       <h2 className="pt-5" data-aos="fade-right" data-aos-duration="800">CONTACTO</h2>
       <div className="bar" data-aos="fade-left" data-aos-duration="800"></div>
       <h5 className="py-1 preguntas text-center" data-aos="fade-up" data-aos-duration="800"><i>¿Alguna pregunta o querés que <span>trabajemos juntos?</span></i></h5>
-      <form action="/" name="contact" method="POST" data-netlify="true" className="form" onSubmit={e => handleSubmit(e)} data-aos="fade-up" data-aos-duration="800">
+      <form action="/" name="contact" method="POST" data-netlify="true" className="form" onSubmit={(e) => handleSubmit(e)} data-aos="fade-up" data-aos-duration="800">
         <div className="row">
           {/* hidden input */}
           <input type="hidden" name="form-name" value="contact" />
