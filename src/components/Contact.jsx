b6b945feb2a6c6c513fe0be2f7b17707f44c0523
Alexis Santa Ana <ibanez2112@hotmail.com>
2022-08-30 20:02:33 -0300

    asdf

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -45,6 +45,8 @@ const Contact = () => {
       <h5 className="py-1 preguntas text-center" data-aos="fade-up" data-aos-duration="800"><i>¿Alguna pregunta o querés que <span>trabajemos juntos?</span></i></h5>
       <form name="contact" method="POST" data-netlify="true" className="form" onSubmit={e => handleSubmit(e)} data-aos="fade-up" data-aos-duration="800">
         <div className="row">
+          {/* hidden input */}
+          <input type="hidden" name="form-name" value="contact" />
           {/* name */}
           <div className="col-12 col-sm-6 d-flex flex-column">
             <label htmlFor="name"><b>Nombre completo</b></label>
