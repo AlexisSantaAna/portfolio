43230811bc19212ca8f0153c5182fb65259731bb
Alexis Santa Ana <ibanez2112@hotmail.com>
2022-11-11 01:37:44 -0300

    added fullstack and mongodb and genexus icons

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -7,7 +7,7 @@ const Profile = () => {
     <div className='d-flex flex-column align-items-center container-profile text-center'>
       <img src="./assets/profilepicblacksquare.jpg" alt="profile pic" className='profile-picture' />
       <h1 className='animate__slow animate__animated animate__fadeInDown'><b>Hola, soy <span>Alexis</span></b></h1>
-      <h4 className='animate__delay-1s animate__slow animate__animated animate__fadeIn'><i>Desarrollador web</i></h4>
+      <h4 className='animate__delay-1s animate__slow animate__animated animate__fadeIn'>Fullstack Developer</h4>
       <p className='profile-text animate__slow animate__animated animate__fadeInUp'>Mi pasión es crear sitios <b>responsivos, intuitivos y dinámicos. </b><br/>¡Te invito a conocer mi trabajo!</p>
       <h2 className='animate__slow animate__animated animate__fadeInUp'>TECNOLOGÍAS</h2>
       <div className='animate__slow animate__animated animate__fadeInUp bar'></div>
